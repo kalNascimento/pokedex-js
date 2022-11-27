e59545b46b42f1702d5e72fb2650506c7bc3dce7
Kalebe Nascimento <kalebe.nascimento19@gmail.com>
2022-11-26 22:53:48 -0300

    Fix stats name width

--- assets/script/redirect.js
+++ assets/script/redirect.js
@@ -5,5 +5,5 @@ if (protocol == 'file:') host = window.location.href.slice(0, 48);
 
 setTimeout(() => {
     console.log('ok')
-    window.location.href = `${host}/pokemons_list.html`;
+    window.location.href = `${host}pokemons_list.html`;
 }, 3000)
